99158cdd0af9ac3c4acfa1b681e5728b3a242c65
Linda Tonui <tonuilinda@gmail.com>
2020-05-10 15:29:59 +0300

    fix indentation error

--- JS/script.js
+++ JS/script.js
@@ -11,137 +11,69 @@ const CURRENTYEAR = 2020;
 const maleAkanName = ['Kwasi','Kwadwo','Kwadena','Kwanu','Yaw','Kofi','Kwame'];
 const femaleAkanName = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 const days = ['Sunday','Monday','Teusday','Wednesday','Thursday','Friday','Saturday'];
-var yearBorn = document.getElementById('year').value;
-var age = parseInt(document.getElementById('age')).value;
-var centuryDigits =parseInt(yearBorn.substring(0,2)) ;
-var yearDigits = parseInt(yearBorn.substring(2,4));
-var dayOfMonth = parseInt(document.getElementById('date')).value;
-var month= parseInt(document.getElementById('month')).value;
-var gender = document.getElementsByName('optionRadios').value;
-
+var yearBorn = document.getElementById("year").value;
+var centuryDigits =Number.parseInt(yearBorn.substring(0,2)) ;
+var yearDigits = Number.parseInt(yearBorn.substring(2,4));
+var age = Number.parseInt(document.getElementById("age").value);
+var dayOfMonth = Number.parseInt(document.getElementById("date").value);
+var month= Number.parseInt(document.getElementById("month").value);
+var gender = document.getElementsByName('optionsRadios');
 
    
 
+function validation() {
+   if (yearBorn===" " || month ===" " || dayOfMonth=== " " || age === " " || CURRENTYEAR-yearBorn !== age || month > 12 || month<0 || dayOfMonth<=0 || dayOfMonth >31){
+    return false;
+   }
+   else{
+    return true;
+   }
+}
 
-function  validation(){
-    if (yearBorn ===" "){
-        alert("Please fill!!");
-        
-    }
-    else if(month === ""){
-        alert("Please fill!!");
-       
-        
-    }
-    else if(dayOfMonth=== ""){
-        alert("Please fill!!");
-        
-    }
-    else if (month > 12|| month<0){
-        alert('Please input a correct month')
-    }
-    else if(CURRENTYEAR-yearBorn !== age){
-        alert("please re-enter your age or year of birth");
-        return false;
-    }
-    else if (dayOfMonth<0 || dayOfMonth >31){
-        alert("Invalid Date.");
-       
+function findDay(dayOfTheWeek){
+    dayOfTheWeek = (((centuryDigits/4)-2*centuryDigits-1)+((5*yearDigits/4))+((26*(month+1)/10))+dayOfMonth)%7;
+    console.log(dayOfTheWeek);
+    return (Math.floor(dayOfTheWeek));
+
+}
+
+
+ function akanName( day,myGender){
+    if (gender[0].checked==true && gender.value==="option1" ) {
+        myGender = "Male";
+   }
+   else  if(gender[1].checked == true && gender.value==="option2") {
+       myGender = "Female";
+   }
+   else if(gender[0].checked==false && gender[1].checked == false){
+   alert('You must pick a gender!!');
     }
     else{
-    return true;
-    }
-}
-var daysOfTheWeek= function() {
+   return true;}
+     
     
-    dayOfTheWeek = ((((centuryDigits/4)- 2*centuryDigits-1)+(5*yearDigits/4)+(26 * (month+1)/10)+dayOfMonth)% 7);
     
-}
- var akanName = function() {
-    validation();
-        
-        if(gender[0].checked==true){
-
-        if (gender.value==="male" ) {
-            
-        
-        switch (daysOfTheWeek) {
-                case 1:
-                alert("You were born on " +days[0] + "and Your Akan Name is " +maleAkanName[0]);
-                break;
-                case 2:
-                alert("You were born on " +days[1] + "and Your Akan Name is " +maleAkanName[1]);
-                break;
-                case 3:
-                alert("You were born on " +days[2] + "and Your Akan Name is " +maleAkanName[2]);
-                break;
-                case 4:
-                alert("You were born on " +days[3] + "and Your Akan Name is " +maleAkanName[3]);
-                break;
-                case 5:
-                alert("You were born on " +days[4] + "and Your Akan Name is " +maleAkanName[4]);
-                break;
-                case 6:
-                alert("You were born on " +days[5] + "and Your Akan Name is " +maleAkanName[5]);
-                break;
-                case 7:
-                alert("You were born on " +days[6] + "and Your Akan Name is " +maleAkanName[6]);
-                break;
-        
-            default:
-                alert('Please Re - enter Yout Information again.');
-                break;
-        }
-    }
-    else if (gender[1].checked = true){
-    if(gender.value=="female") {
-        switch (daysOfTheWeek) {
-            case 1:
-            alert("You were born on " +days[0] + "and Your Akan Name is " +femaleAkanName[0]);
+    switch (myGender) {
+        case "Male":
+            document.getElementById('result').textContent ="You were born on " +days[day] + "and Your Akan Name is " +maleAkanName[day];
             break;
-            case 2:
-            alert("You were born on " +days[1] + "and Your Akan Name is " +femaleAkanName[1]);
+        case "Female":
+            document.getElementById('result').textContent ="You were born on " +days[day] + "and Your Akan Name is " +femaleAkanName[day];
             break;
-            case 3:
-            alert("You were born on " +days[2] + "and Your Akan Name is " +femaleAkanName[2]);
-            break;
-            case 4:
-            alert("You were born on " +days[3] + "and Your Akan Name is " +femaleAkanName[3]);
-            break;
-            case 5:
-            alert("You were born on " +days[4] + "and Your Akan Name is " +femaleAkanName[4]);
-            break;
-            case 6:
-            alert("You were born on " +days[5] + "and Your Akan Name is " +femaleAkanName[5]);
-            break;
-            case 7:
-            alert("You were born on " +days[6] + "and Your Akan Name is " +femaleAkanName[6]);
-            break;
-    
         default:
-            alert('Please Re - enter Your Information again.');
-            break;
-    }
-}
+            alert('Please Re - enter Your Information again.')
+          }  
+        }
 
-    }
-    else {
-        alert("You must Pick a gender!!!")
-    }
-}
-}
 
 function clear(){
     document.getElementByClass('form-control').innerHTML = "";
 }
 
-            
-        
-
-    function getMyAkan() {
-    
-        akanName ();
-        
-    }
-
+function getMyName(){
+    var day = findDay();
+    validation();
+    akanName();
+  
+}
 
